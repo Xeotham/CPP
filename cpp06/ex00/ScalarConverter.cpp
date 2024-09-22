4694b3f90648399d57bf36d9f872a377854c9c3a
mhaouas <mat.haouas@gmail.com>
2024-09-22 23:05:59 +0200

    Need to test cpp06 ex00

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -6,11 +6,12 @@
 /*   By: mhaouas <mhaouas@student.42angouleme.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 18:48:29 by mhaouas           #+#    #+#             */
-/*   Updated: 2024/09/20 19:01:31 by mhaouas          ###   ########.fr       */
+/*   Updated: 2024/09/22 20:52:36 by mhaouas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
+#include <climits>
 
 ScalarConverter::ScalarConverter()
 {
@@ -31,5 +32,120 @@ ScalarConverter	&ScalarConverter::operator=(const ScalarConverter &new_scalar)
 
 void	ScalarConverter::convert(const std::string str)
 {
-	// if (str.size() == 1 && )
+	if (str.size() == 1 && std::isprint(str.at(0)))
+		is_char(str.at(0));
+	else if (str.size() > 1 && check_param(str) == IS_FLOAT)
+		is_float(std::stof(str.c_str()));
+	else if (check_param(str) == IS_DOUBLE)
+		is_double(std::stod(str));
+	else if (check_param(str) == IS_INT)
+		is_int(std::stoi(str));
+	else if (str == "-inff" || str == "+inff" || str == "-inf" || str == "+inf")
+		is_inf(str);
+	else if (str == "nanf" || str == "nan")
+		is_nan(str);
+	else
+		std::cerr << "Error : wrong parameter." << std::endl;
+}
+
+void	ScalarConverter::is_char(char c)
+{
+	std::cout << "Value in char :	" << c << "." << std::endl;
+	std::cout << "Value in int :	" << static_cast<int>(c) << "." << std::endl;
+	std::cout << "Value in float :	" << static_cast<float>(c) << "f." << std::endl;
+	std::cout << "Value in double :	" << static_cast<double>(c) << "." << std::endl; 
+}
+
+void	ScalarConverter::is_int(int nb)
+{
+	if (nb < 0 || nb > 127)
+		std::cout << "Value in char :	Impossible." << std::endl;
+	else
+		std::cout << "Value in char :	" << static_cast<char>(nb) << "." << std::endl;
+	std::cout << "Value in int :	" << nb << "." << std::endl;
+	std::cout << "Value in float :	" << static_cast<float>(nb) << "f." << std::endl;
+	std::cout << "Value in double :	" << static_cast<double>(nb) << "f." << std::endl;
+}
+
+void	ScalarConverter::is_float(float nb)
+{
+	if (nb < 0 || nb > 127)
+		std::cout << "Value in char :	Impossible." << std::endl;
+	else
+		std::cout << "Value in char :	" << static_cast<char>(nb) << "." << std::endl;
+	if (nb < static_cast<float>(INT_MIN) || nb > static_cast<float>(INT_MAX))
+		std::cout << "Value in int :	Impossible." << std::endl;
+	else
+		std::cout << "Value in int :	" << static_cast<int>(nb) << "." << std::endl;
+	std::cout << "Value in float :	" << nb << "f." << std::endl;
+	std::cout << "Value in double :	" << static_cast<double>(nb) << "." << std::endl;
+}
+
+void	ScalarConverter::is_double(double nb)
+{
+	if (nb < 0 || nb > 127)
+		std::cout << "Value in char :	Impossible." << std::endl;
+	else
+		std::cout << "Value in char :	" << static_cast<char>(nb) << "." << std::endl;
+	if (nb < static_cast<double>(INT_MIN) || nb > static_cast<double>(INT_MAX))
+		std::cout << "Value in int :	Impossible." << std::endl;
+	else
+		std::cout << "Value in int :	" << static_cast<int>(nb) << "." << std::endl;
+	if (nb < static_cast<double>(-__FLT_MAX__) || nb > static_cast<double>(__FLT_MAX__))
+		std::cout << "Value in float :	Impossible." << std::endl;
+	else
+		std::cout << "Value in float :	" << static_cast<double>(nb) << "f." << std::endl;
+	std::cout << "Value in double :	" << nb << "." << std::endl;
+}
+
+int	ScalarConverter::check_param(std::string str)
+{
+	for (std::string::iterator iter = str.begin(); iter <= str.end(); iter++)
+	{
+		if (iter == str.end() && *iter == 'f')
+			break;
+		if (!std::isdigit(*iter) && *iter != '.')
+			return (NOT_PARAM);
+	}
+	if (str.find('.') == std::string::npos)
+		return (IS_INT);
+	else if (str.find_first_of('.') != str.find_last_of('.'))
+		return (NOT_PARAM);
+	else if (str.back() == 'f')
+		return (IS_FLOAT);
+	else
+		return (IS_DOUBLE);
+
+}
+
+void	ScalarConverter::is_inf(std::string str)
+{
+	std::cout << "Value in char :	Impossible." << std::endl;
+	std::cout << "Value in int :	Impossible." << std::endl;
+	if (str == "-inff" || str == "+inff")
+	{
+		std::cout << "Value in float :	" << str << "." << std::endl;
+		std::cout << "Value in double :	Impossible." << std::endl;
+	}
+	else
+	{
+		std::cout << "Value in float :	Impossible." << std::endl;
+		std::cout << "Value in double :	" << str << "." << std::endl;
+	}
+}
+
+void ScalarConverter::is_nan(std::string str)
+{
+	std::cout << "Value in char :	Impossible." << std::endl;
+	std::cout << "Value in int :	Impossible." << std::endl;
+	if (str == "nanf")
+	{
+		std::cout << "Value in float :	nanf." << std::endl;
+		std::cout << "Value in double :	Impossible." << std::endl;
+	}
+	else
+	{
+		std::cout << "Value in float :	Impossible." << std::endl;
+		std::cout << "Value in double :	nan." << std::endl;
+	}
 }
--- cpp06/ex00/ScalarConverter.hpp
+++ cpp06/ex00/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: mhaouas <mhaouas@student.42angouleme.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 18:47:00 by mhaouas           #+#    #+#             */
-/*   Updated: 2024/09/20 18:59:39 by mhaouas          ###   ########.fr       */
+/*   Updated: 2024/09/22 20:41:58 by mhaouas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,22 @@ class ScalarConverter
 		ScalarConverter();
 		ScalarConverter(const ScalarConverter &new_scalar);
 		ScalarConverter	&operator=(const ScalarConverter &new_scalar);
-		
+	private:
+		static void	is_char(char c);
+		static void	is_int(int i);
+		static void	is_float(float f);
+		static void	is_double(double d);
+		static void	is_inf(std::string str);
+		static void	is_nan(std::string str);
+		static int	check_param(const std::string str);
+	private:
+		enum	e_param
+		{
+			NOT_PARAM = -1,
+			IS_INT,
+			IS_FLOAT,
+			IS_DOUBLE
+		};
 };
 
 
